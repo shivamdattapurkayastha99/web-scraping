7200aa9b5919d6f22f953cb9c6f5b43299cba81d
shivam <shivamdatta465@gmail.com>
2020-08-17 00:42:30 +0530

    first commit

---
+++ scraping.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+url="https://www.codewithharry.com"
+r=requests.get(url)
+htmlcontent=r.content
+soup=BeautifulSoup(htmlcontent,'html.parser')
+title=soup.title
+paras=soup.find_all('p')
+anchors=soup.find_all('a')
+# print(soup.find('p'))
+# print(soup.find('p')['class'])
+# print(soup.find_all('p',class_='lead'))
+# print(soup.find('p').get_text())
+# print(soup.get_text())
+all_links=set()
+
+for link in anchors:
+    if link.get('href')!="#":
+        link_Text="https://www.codewithharry.com"+link.get('href')
+        all_links.add(link_Text)
+        # print(link_Text)
+navbarSupportedContent=soup.find(id="navbarSupportedContent")
+# print(navbarSupportedContent.contents)
+# for ele in navbarSupportedContent.contents:
+#     
+    # print(ele)
+# .contents - A tag's children are available as list
+# .children - A tag's children are available as generator
+# for item in navbarSupportedContent.strings:
+#     print(item)
+# for item in navbarSupportedContent.stripped_strings:
+#     print(item)
+# print(navbarSupportedContent.parent)
+# for item in navbarSupportedContent.parents:
+#     print(item.name)
+# print(navbarSupportedContent.next_sibling.next_sibling)
+# print(navbarSupportedContent.prev_sibling.prev_sibling)
